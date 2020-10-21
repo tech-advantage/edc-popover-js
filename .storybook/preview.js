99d35bbedfd99a4efeaf65bad228ef3fc1af2743
Daniel Mayer <daniel.mayer@tech-advantage.com>
2020-10-21 10:59:09 +0200

    [EDC-2691] Add storybook v6
    - include a mini doc
    - downgrade webpack to v4 due to incompatibility with storybook (https://github.com/storybookjs/storybook/issues/9216)

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+// For ordering storybook roots, ordered by title identifiers
+// https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy
+export const parameters = {
+    options: {
+        storySort: {
+            method: '',
+            order: ['Edc main properties', 'Edc popover options', 'Edc popover fail behavior', 'Edc custom style'],
+            locales: '',
+        },
+    },
+};
---
+++ src/stories/edc-popover-fail-behavior.stories.ts
@@ -0,0 +1,38 @@
+import { EdcPopoverOptions, EdcProperties, FailBehavior } from '../class';
+import EdcPopover from '../index';
+import { IconBehavior, PopoverBehavior } from '../class/fail-behavior';
+import { edcConfig } from './edc-config';
+
+const HelpFailBehavior = {
+    title: 'Edc popover fail behavior',
+    argTypes: {
+        icon: {
+            control: {
+                type: 'select',
+                options: Object.values(IconBehavior)
+            }
+        },
+        popover: {
+            control: {
+                type: 'select',
+                options: Object.values(PopoverBehavior)
+            }
+        }
+    }
+};
+
+export default HelpFailBehavior;
+
+const defaultValues = { icon: IconBehavior.SHOWN, popover: PopoverBehavior.FRIENDLY_MSG  };
+const Template = ({ icon, popover }) => {
+    const options = new EdcPopoverOptions();
+    options.failBehavior = { ...new FailBehavior(), icon, popover };
+    const edcProperties = { mainKey:'fr.techad.edc', subKey: 'error.subkey', options };
+    const container = document.createElement('div');
+    EdcPopover.config(edcConfig);
+    EdcPopover.create(container, edcProperties);
+    return container;
+};
+
+export const EdcHelpFailBehavior = Template.bind({});
+EdcHelpFailBehavior.args = defaultValues;
